c85189389339f91a6ea1735b08a34e5b7714cb0f
Sakshi Srivastava <srivsaks@amazon.com>
2022-02-23 19:10:38 +0530

    Simple react drag and drop list(one single list is present)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
 
 function App() {
+  const[list,setList]=useState(["Task1","Task2","Task3","Task4","Task5","Task6"])
+  const[draggedItem,setDraggedItem]=useState(null);
+
+  const handleDragStart=(e,index)=>{
+  setDraggedItem(list[index]);
+  //e.dataTransfer.effectAllowed="move";
+  e.dataTransfer.setData("text",e.target.parentNode)
+
+  /*setTimeout(()=>{
+    e.target.style.display="none"
+  },0)*/
+  
+  }
+
+  const handleDragOver=(e,index)=>{
+    e.preventDefault();
+    const draggedOverItem=list[index];
+    if(draggedOverItem===draggedItem){
+      return;
+    }
+    const filteredItems=list.filter((item)=>{
+      return item!==draggedItem
+    })
+
+    filteredItems.splice(index,0,draggedItem);
+    setList(filteredItems);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <h3>Drag and Drop Project</h3>
+         <ul>
+           {list.map((item,index)=>(
+             <li className='item-style' onDragOver={(e)=>{handleDragOver(e,index)}}>
+            <div draggable onDragStart={(e)=>{handleDragStart(e,index)}}>
+            {item}
+            </div>
+             </li>
+           ))}
+         </ul>
       </header>
     </div>
   );
